50c714ab4d0434b1e6711de6a6b0ffcb5317a8f9
Jelmer Kuperus <jkuperus@ebay.com>
2019-10-14 09:44:15 +0200

    Set the pir sensor to most active and handle how long we light up in code

--- src/main/scala/com/github/jelmer/luminary/OrchestratorActor.scala
+++ src/main/scala/com/github/jelmer/luminary/OrchestratorActor.scala
@@ -1,11 +1,15 @@
 package com.github.jelmer.luminary
 
+
+import scala.concurrent.duration._
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import com.github.jelmer.luminary.LedStripActor.{TurnOff, TurnOn}
-import com.github.jelmer.luminary.PirSensorActor.{MotionDetected, NoMotionDetected, Subscribe}
-import com.github.jelmer.luminary.SpeakerActor.Play
+import com.github.jelmer.luminary.PirSensorActor.{MotionDetected, Subscribe}
+import com.github.jelmer.luminary.SpeakerActor.{Play, StopPlayback}
 
 object OrchestratorActor {
+
+  case object TriggerEnd
 
   def props(ledStripActor: ActorRef, pirSensorActor: ActorRef, speakerActor: ActorRef): Props =
     Props(new OrchestratorActor(ledStripActor, pirSensorActor, speakerActor))
@@ -14,22 +18,34 @@ object OrchestratorActor {
 class OrchestratorActor(ledStripActor: ActorRef, pirSensorActor: ActorRef, speakerActor: ActorRef)
   extends Actor with ActorLogging {
 
-  private val mediaUrl = Thread.currentThread().getContextClassLoader.getResource("alert.wav").toString
+  import context.dispatcher
+  import OrchestratorActor._
+
+  private val mediaUrl = Thread.currentThread.getContextClassLoader.getResource("alert.wav").toString
 
   override def preStart(): Unit = {
     pirSensorActor ! Subscribe
   }
 
-  override def receive: Receive = {
+  def active: Receive = {
+    case TriggerEnd =>
+      log.info("Trigger end")
+      speakerActor ! StopPlayback
+      ledStripActor ! TurnOff
+      context.become(inactive)
+  }
+
+  def inactive: Receive = {
     case MotionDetected =>
       log.debug("Motion detected. Turning on light.")
 
       speakerActor ! Play(mediaUrl)
       ledStripActor ! TurnOn
 
-    case NoMotionDetected =>
-      log.debug("Motion detected. Turning off light.")
+      context.system.scheduler.scheduleOnce(10.second, self, TriggerEnd)
 
-      ledStripActor ! TurnOff
+      context.become(active)
   }
+
+  override def receive: Receive = inactive
 }
--- src/main/scala/com/github/jelmer/luminary/SpeakerActor.scala
+++ src/main/scala/com/github/jelmer/luminary/SpeakerActor.scala
@@ -1,15 +1,11 @@
 package com.github.jelmer.luminary
 
-import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
-import akka.stream.ActorMaterializer
-import com.github.jelmer.luminary.Main.system
-import javafx.beans.value
-import javafx.beans.value.ObservableValue
+import akka.actor.{Actor, ActorLogging, Props}
 import javafx.scene.media.{Media, MediaPlayer}
 
-import scala.concurrent.ExecutionContext
-
 object SpeakerActor {
+  case object StopPlayback
+
   case class Play(url: String)
 
   case object EndOfMedia
@@ -18,6 +14,7 @@ object SpeakerActor {
 
   def props: Props = Props[SpeakerActor]
 }
+
 class SpeakerActor extends Actor with ActorLogging {
 
   import SpeakerActor._
@@ -25,16 +22,18 @@ class SpeakerActor extends Actor with ActorLogging {
   def playing(mediaPlayer: MediaPlayer): Receive = {
 
     case Play(url) =>
-      sender ! Busy
       log.info(s"Ignoring request to play $url. already playing")
-    case EndOfMedia =>
-      log.debug("End of media file reached becoming idle")
+      sender ! Busy
+
+    case EndOfMedia | StopPlayback =>
+      log.info("Stopping audio playback")
       mediaPlayer.dispose()
       context.become(idle)
   }
 
   def idle: Receive = {
     case Play(url) =>
+      log.info(s"Playing $url")
       val hit = new Media(url)
       val mediaPlayer = new MediaPlayer(hit)
       mediaPlayer.setOnEndOfMedia(() =>  self ! EndOfMedia)
@@ -44,40 +43,4 @@ class SpeakerActor extends Actor with ActorLogging {
 
   override def receive: Receive = idle
 
-}
-
-
-object Test {
-
-
-  def main(args: Array[String]): Unit = {
-
-    import SpeakerActor._
-
-//    val hit = new Media(new File(bip).toURI.toString)
-    com.sun.javafx.application.PlatformImpl.startup(() => ())
-
-
-    implicit val system: ActorSystem = ActorSystem()
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
-    implicit val executor: ExecutionContext = system.dispatcher
-
-
-    val speakerActor: ActorRef = system.actorOf(SpeakerActor.props, "speakerActor")
-
-    val mediaUrl = Thread.currentThread().getContextClassLoader.getResource("alert.wav").toString
-
-    speakerActor ! Play(mediaUrl)
-
-    Thread.sleep(10000)
-
-    speakerActor ! Play(mediaUrl)
-
-    Thread.sleep(1000)
-
-    speakerActor ! Play(mediaUrl)
-
-    Thread.sleep(1000000)
-
-  }
 }
